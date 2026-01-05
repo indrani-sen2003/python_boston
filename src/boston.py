c45bccb7631ec2eede7375ef3d31c9c929e7fde2
indrani-sen2003 <indrani.sen@udcs.mu.ac.in>
2026-01-05 09:06:06 +0530

    Update boston.py

--- src/boston.py
+++ src/boston.py
@@ -5,10 +5,11 @@ from sklearn.metrics import r2_score
 import pickle
 def model_reg_boston(file):
     df=pd.read_csv(file)
-    x=df.drop("MEDV",axis=1)
-    y=df["MEDV"]
+    x=df.drop("medv",axis=1)
+    y=df["medv"]
     x_tr,x_test,y_tr,y_test=train_test_split(x,y,test_size=0.2)
     model=LinearRegression().fit(x_tr,y_tr)
     p=model.predict(x_test)
     r2=r2_score(y_test,p)
     return r2
+
