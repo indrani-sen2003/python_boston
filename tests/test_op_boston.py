fab83f39a2836b376997df6ff70a311b9124f914
indrani-sen2003 <indrani.sen@udcs.mu.ac.in>
2026-01-05 09:03:30 +0530

    Update test_op_boston.py

--- tests/test_op_boston.py
+++ tests/test_op_boston.py
@@ -2,11 +2,15 @@ import sys
 import os
 project_root=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, project_root)
+from pathlib import Path
+import pandas as pd
 
+DATA_PATH = Path(__file__).parent / "BostonHousing.csv"
 
 from src.boston import model_reg_boston
 
 def test_ac():
-    assert model_reg_boston("BostonHousing.csv")>0.7
+    assert model_reg_boston(DATA_PATH)>0.7
+
 
 
